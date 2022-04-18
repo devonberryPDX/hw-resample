9d7a91ad12ce24bd68623351d97e002a29b52885
Devon Berry <devberry@pdx.edu>
2022-04-18 12:59:20 -0700

    'working'

--- halfrate.py
+++ halfrate.py
@@ -8,8 +8,6 @@ import sounddevice as sd
 import soundfile as sf
 
 from scipy import signal
-from scipy.signal import convolve as sig_convolve
-import numpy as np
 
 #-----------------------------------------------------------------------
 # Argument parser
@@ -46,25 +44,10 @@ event = threading.Event()
 # Guide I used: https://scipy-cookbook.readthedocs.io/items/ApplyFIRFilter.html?highlight=half%20band%20filter
 data, fs = sf.read(args.filename, always_2d=True)
 
-with open('coeffs.txt') as f:
-    w = [float(x) for x in next(f).split()] # read first line
-    array = np.empty(91)
-    for line in f: # read rest of lines
-        np.append(array, [float(x) for x in line.split()])
-
-#coefficients = open("coeffs.txt", "r")
-#lines = coefficients.read().split('\n')
-#print(lines)
-
-l = array[5]
-print(len(array))
-
 nopt, bopt = signal.kaiserord(-40, 0.05)
 subband = signal.firwin(1, 0.45, window=('kaiser', bopt), scale=True)
 
-#filteredData = signal.lfilter(array, [1.0], data)[:, len(array) - 1:]
-filteredData = sig_convolve(data, array[np.newaxis, :])
-
+filteredData = signal.lfilter(subband, [1.0], data)[:, len(subband) - 1:]
 
 decimationData = filteredData[::2]
 
